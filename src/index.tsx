61c80cde6e13ead92a760a865423b9882ced6278
ByteCntrl <bytecntrl@gmail.com>
2023-06-16 11:35:57 +0200

    Add react-query

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { RouterProvider } from "react-router-dom";
 
 import routers from './router';
 
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
+const queryClient = new QueryClient();
 
 root.render(
     <React.StrictMode>
-        <RouterProvider router={routers} />
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={routers} />
+        </QueryClientProvider>
     </React.StrictMode>
 );
